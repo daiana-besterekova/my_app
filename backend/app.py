e821f3fd8acfc1f844cfe6a57aa69426504eb8b1
daiana-besterekova <85840329+daiana-besterekova@users.noreply.github.com>
2023-07-12 01:57:20 +0600

    implement image generaiton and color picker

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI, Request, Form
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
 import openai
 import uvicorn
+from color import *
+
+class ImageDescription(BaseModel):
+    description: str
+    color: str
 
 class Settings(BaseModel):
-    OPENAI_API_KEY: str = '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'
-
+    OPENAI_API_KEY: str = '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'
     class Config:
         env_file = '.venv'
 
@@ -19,18 +21,19 @@ app = FastAPI()
 @app.post("/generate_image")
 async def generate_image(desc: ImageDescription):
     response = openai.Image.create( 
-        prompt=generate_prompt(desc.description),
+        prompt=generate_prompt(desc.description, desc.color),
         n=1,
         size="256x256"
     )
     result = response['data'][0]['url']
     return {"url": result}
 
-
-def generate_prompt(description):
+def generate_prompt(description, color):
+    color = color if color else "rgb(255,255,255)" # default color is white
+    color = closest_remove(color)
+    print(color)
     text = description if description else "some default prompt"
-    return f"A sunlit indoor lounge area with a pool containing a {text}"
-
+    return f"{text} for background сolor use this {color} color"
 
 if __name__ == "__main__":
     uvicorn.run('app:app', host="localhost", port=5001, reload=True)
